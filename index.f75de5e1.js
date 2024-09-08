d39c44433300f50c951b9daec0c5f17622f23ed0
Serhii Babskyi <sergbald99@gmail.com>
2024-09-08 21:28:36 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const mainImg = document.getElementById("largeImg");
+const thumbs = document.getElementById("thumbs");
+thumbs.addEventListener("click", handler);
+function handler(e) {
+    e.preventDefault();
+    const thumb = e.target.closest("a");
+    if (!thumb) return;
+    const src = thumb.href;
+    const alt = thumb.title;
+    mainImg.src = src;
+    mainImg.alt = alt;
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
